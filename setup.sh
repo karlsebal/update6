75a8daf1f1bc5496c11d3546e43d4bb5ceb64d08
karlsebal <karlsebal@posteo.de>
2015-04-22 16:52:29 +0200

    added disable/enable

--- setup.sh
+++ setup.sh
@@ -14,12 +14,14 @@ case "$action" in
 	[ -n "$(ps | grep update6 | grep -v grep)" ] && { $warn_run; $advice; exit; }
 	cp -i etc/config/update6 /etc/config/
 	cp -i etc/init.d/update6 /etc/init.d/
+	cp -i etc/hotplug.d/iface/25-update6
 	cp -i usr/bin/update6 /usr/bin/
    ;;
 
    remove)
 	/etc/init.d/update6 stop
 	/etc/init.d/update6 disable
+	rm /etc/hotplug.d/iface/25-update6
 	rm /etc/config/update6
 	rm /etc/init.d/update6
 	rm /usr/bin/update6
