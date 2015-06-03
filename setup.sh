f59bc6ae713fb5561129242760191c6c3e2f68c0
karlsebal <karlsebal@posteo.de>
2015-06-03 12:20:41 +0200

    filter out setup when lookup running

--- setup.sh
+++ setup.sh
@@ -11,10 +11,10 @@ case "$action" in
 
   install)
 	[ -f /var/run/update6.pid ] && { $warn_pid; $advice; exit; }
-	[ -n "$(ps | grep update6 | grep -v grep)" ] && { $warn_run; $advice; exit; }
+	[ -n "$(ps | grep update6 | grep -v grep | grep -v $$)" ] && { $warn_run; $advice; exit; }
 	cp -i etc/config/update6 /etc/config/
 	cp -i etc/init.d/update6 /etc/init.d/
-	cp -i etc/hotplug.d/iface/25-update6
+	cp -i etc/hotplug.d/iface/25-update6 /etc/hotplug.d/iface/25-update6
 	cp -i usr/bin/update6 /usr/bin/
    ;;
 
